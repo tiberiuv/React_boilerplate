63f92f6b2b42b6dca11579a785821c74bce157c1
Tiberiu Simion Voicu <ts.voicu@icloud.com>
2021-05-31 13:27:49 +0100

    fix ci 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-        'prettier/@typescript-eslint',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
