40940cda2b1530eef69ebd06a2fcde6f33515d23
Tiberiu Voicu <ts.voicu@icloud.com>
2022-02-11 23:01:05 +0000

    upgrade packages

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,34 @@
-import {ReactElement} from 'react'
+import { ReactElement, useReducer } from 'react'
 import styled from '@emotion/styled'
 import globalStyle from './globalStyle'
-import {Global} from '@emotion/react'
+import { Global } from '@emotion/react'
 
 const Container = styled.h1`
     margin: auto;
     color: blue;
 `
 
-const Wrapper = <T,>({text}: {text: T}) => <Container>{text}</Container>
+const Wrapper = <T,>({ text }: { text: T }) => <Container>{text}</Container>
+
+type ActionType = 'RUN_REQUEST'
+
+type Request<T> = {
+    fn: () => Promise<T>
+}
+type State<T> = {
+    groups: { [id: string]: Request<T>[] }
+}
+
+type Action<T> = {
+    type: 'RUN_REQUEST',
+    fn: () => Promise<T>
+}
+
+const reducer = <T,>(state: State<T>, action: Action<T>) => {
+}
 
 const App = (): ReactElement => {
+    const [state, dispatch] = useReducer<State<String>, Action<String>>()
     return (
         <>
             <Global styles={globalStyle} />
